48758b0b3b96b9d15e2883185a12284de69b0ed1
Clarence <cobonibon@gmail.com>
2026-05-06 12:07:12 +0800

    mainWindow Setting
    Setting the mainWindow and made markers for future developments

--- Code/BTP-BLS.py
+++ Code/BTP-BLS.py
@@ -1,6 +1,35 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk        # we cant use `*` as it is not recomended due to the posiblity of library conflicts.
+from tkinter import ttk     # Extra Features of tkinter
+import os                   # file handling
 
-mainWindow = Tk()
+# functions here
 
+#####################################################################################################
+# file handling
+def logoFileNameConstant():
+    """Gets the file path of the logo"""
+    mainDirectory = os.path.dirname(os.path.abspath(__file__))
+    logoPath = os.path.join(mainDirectory, "..", "images", "logo.png")
+    filenameConstant = os.path.normpath(logoPath)
+    return filenameConstant
+
+#####################################################################################################
+# window(root) settings
+mainWindow = tk.Tk()
+mainWindow.title("BTP-BLS")
+mainWindow.geometry("600x400")
+logo = tk.PhotoImage(file = logoFileNameConstant())
+mainWindow.iconphoto(True, logo)
+
+#####################################################################################################
+# Page 1: Translation Page
+
+#####################################################################################################
+# Page 2: Game Activity
+
+#####################################################################################################
+# Page 3: Chart
+
+#####################################################################################################
+# Mainloop
 mainWindow.mainloop()
