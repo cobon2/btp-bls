29819270597e214621bcf61063829eb2bc120c0e
Clarence <cobonibon@gmail.com>
2026-05-06 16:58:31 +0800

    mainNotebook Creation
    there, the notebook has been added.

--- Code/BTP-BLS.py
+++ Code/BTP-BLS.py
@@ -21,17 +21,27 @@ mainWindow.geometry("600x400")
 logo = tk.PhotoImage(file = logoFileNameConstant())
 mainWindow.iconphoto(True, logo)
 
+## notebook declaration
+mainNotebook = ttk.Notebook(mainWindow)
+mainNotebook.pack(expand=True)
+
 #####################################################################################################
 # Page 1: Translation Page
+placeholderTranslationPage = ttk.Frame(mainNotebook, width=1200, height = 1120) # Placeholder
+placeholderTranslationPage.pack(fill='both', expand=True)
 
 #####################################################################################################
 # Page 2: Game Activity
 
 #####################################################################################################
 # Page 3: Chart
+placeholderChartPage = ttk.Frame(mainNotebook, width=1200, height = 1120) # Placeholder
+placeholderChartPage.pack(fill='both', expand=True)
 
 #####################################################################################################
 # mainNotebook Compiling
+mainNotebook.add(placeholderTranslationPage, text='translation')
+mainNotebook.add(placeholderChartPage, text='translation')
 
 #####################################################################################################
 # Mainloop
