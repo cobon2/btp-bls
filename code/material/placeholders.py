e4cdf453a99059451d899dea3724c16719e12eae
Clarence <cobonibon@gmail.com>
2026-05-22 08:37:56 +0800

    hardfix

--- code/material/placeholders.py
+++
@@ -1,8 +0,0 @@
-import matplotlib as plot
-
-class StatisticImage:
-    def __init__(self):
-        self.WinLoseList = [5, 2, 3, 7, 3]
-        pass
-    def image(self, WinlLoseList):
-        pass
